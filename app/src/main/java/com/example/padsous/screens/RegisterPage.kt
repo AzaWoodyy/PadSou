2c48be135637746f6b20309227cabc33c082f57d
NoahRazzaq <noahrazzaq@live.fr>
2023-01-17 09:00:11 +0100

    :wrench: initialize inputs

---
+++ app/src/main/java/com/example/padsous/screens/RegisterPage.kt
@@ -0,0 +1,10 @@
+package com.example.padsous.screens
+
+import androidx.compose.runtime.Composable
+import com.example.padsous.features.homepage.RegisterPageForm
+
+
+@Composable
+fun RegisterPage() {
+    RegisterPageForm()
+}
